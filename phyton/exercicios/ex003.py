3ca64abd89ce5a154fd19b456b76b3638e5aaecf
Duck-mad <israel.datha@gmail.com>
2023-06-19 21:39:27 -0300

    ok

---
+++ phyton/exercicios/ex003.py
@@ -0,0 +1,7 @@
+valor1 = int(input('Coloque o valor de A: '))
+
+valor2 = int(input('Coloque o valor de B: '))
+
+valor3 = int(input('Coloque o valor de C: '))
+
+print('Aqui está a soma entre {} {} {}: é {}'.format(valor1, valor2, valor3, valor1 + valor2+ valor3))
